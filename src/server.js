ae66e7fc55694287e08f0f1a4b20e4b7bdfd2c7d
Mongark <mongarkprogrammer@gmail.com>
2022-08-18 14:07:21 -0400

    Managed to connect to MongoDB

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express, { json } from 'express';
 import mongoose from 'mongoose';
-import { config } from 'dotenv';
 
-const mongoString = process.env.DATABASE_URL
+const mongodb_url = "mongodb://127.0.0.1:27017/";
+
+mongoose.connect(mongodb_url);
+const database = await mongoose.connection
 
 const app = express();
 
