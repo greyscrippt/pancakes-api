83a89695a7281ac846f6b2dac9136e2a5781e461
Mongark <mongarkprogrammer@gmail.com>
2022-08-18 18:09:04 -0400

    Refactored logging into its own folder

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express, { json } from 'express';
 import mongoose from 'mongoose';
 
+import addToLogs from './logging/logger.js';
+
 import routes from './routes/routes.js';
 
 const startupMessage = "\n\n\n" +
@@ -19,24 +21,6 @@ const startupMessage = "\n\n\n" +
 "    ╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝ " + '\n\n' +
 "    Authored by: Mongark\n\n\n";
 
-const monad = (logs, msg) => {
-    const currentLogId = logs.length;
-
-    console.log(
-        'Stage ' + currentLogId + ': ' +
-        msg
-    );
-
-    return [
-        ...logs,
-        {
-            id: currentLogId,
-            message: msg,
-        }
-    ];
-};
-var logs = [];
-const addToLogs = (msg) => (logs = monad(logs, msg));
 
 console.log(startupMessage);
 addToLogs('Initializing API server.');
