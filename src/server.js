600097c9acfb53d028a6531251767d5ee7298391
Mongark <mongarkprogrammer@gmail.com>
2022-08-18 17:55:44 -0400

    Added logging with monads

--- src/server.js
+++ src/server.js
@@ -3,24 +3,67 @@ import mongoose from 'mongoose';
 
 import routes from './routes/routes.js';
 
-const mongodb_url = "mongodb://127.0.0.1:27017/";
+const startupMessage = "\n\n\n" +
+"    ███╗░░░███╗██╗███╗░░██╗██╗  ███████╗██████╗░██████╗░  ░█████╗░██████╗░██╗ " + '\n' +
+"    ████╗░████║██║████╗░██║██║  ██╔════╝██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██║ " + '\n' +
+"    ██╔████╔██║██║██╔██╗██║██║  █████╗░░██████╔╝██████╔╝  ███████║██████╔╝██║ " + '\n' +
+"    ██║╚██╔╝██║██║██║╚████║██║  ██╔══╝░░██╔══██╗██╔═══╝░  ██╔══██║██╔═══╝░██║ " + '\n' +
+"    ██║░╚═╝░██║██║██║░╚███║██║  ███████╗██║░░██║██║░░░░░  ██║░░██║██║░░░░░██║ " + '\n' +
+"    ╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝ " + '\n\n' +
 
+"    ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░ " + '\n' +
+"    ██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗ " + '\n' +
+"    ╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝ " + '\n' +
+"    ░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗ " + '\n' +
+"    ██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║ " + '\n' +
+"    ╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝ " + '\n\n' +
+"    Authored by: Mongark\n\n\n";
+
+const monad = (logs, msg, lType) => {
+    const currentLogId = logs.length;
+
+    console.log('[' + lType + '] ' + 'Stage ' + currentLogId + ': ' + msg);
+
+    return [
+        ...logs,
+        {
+            id: currentLogId,
+            message: msg,
+            logType: lType,
+        }
+    ];
+};
+var logs = [];
+const addToLogs = (msg) => (logs = monad(logs, msg));
+
+console.log(startupMessage);
+addToLogs('Initializing API server.');
+
+addToLogs('Loading MongoDB database url...');
+const mongodb_url = "mongodb://127.0.0.1:27017/";
+addToLogs('Creating express instance...');
+const app = express();
+addToLogs('Express instance created sucessfully!');
+
+addToLogs('Connecting to MongoDB with URL...');
 mongoose.connect(mongodb_url);
+addToLogs('Creating database client instance...');
 const database = mongoose.connection
 
-const app = express();
+addToLogs('Loading API routes...');
 app.use('/api', routes)
 
 database.on('error', (error) => {
+    addToLogs(error);
     console.log(error)
 })
 
 database.once('connected', () => {
-    console.log('Database Connected');
+    addToLogs('Database connection established!');
 })
 
 app.use(json());
 
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    addToLogs(`Server Started at ${3000}`)
 })
