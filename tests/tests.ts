a10d35a33099badedb26dbfe09bc7985980b1a30
Mongark <mongarkprogrammer@gmail.com>
2022-08-29 15:49:07 -0400

    :white_check_mark: Created test for room.getCount endpoint :white_check_mark:

--- tests/tests.ts
+++ tests/tests.ts
@@ -1,11 +1,35 @@
 import { expect } from "chai";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 import "mocha";
 
-const API_URL = "localhost:3000/api/";
+const API_URL = "http://localhost:3000/api/";
 
 describe("Testing endpoint /rooms/getCount", () => {
-  it("should return all rooms", () => {
-    expect(true).to.equal(true);
+  it("should return number of rooms", async () => {
+    const get = async() => {
+        const c1 = axios.get(API_URL+'rooms/getCount')
+            .then(function (response: AxiosResponse) {
+                // handle success
+                return response.data;
+            })
+            .catch(function (error) {
+                expect(true).to.equal(false);
+            });
+        
+        const c2 = axios.get(API_URL+'rooms/getAll')
+            .then(function (response: AxiosResponse) {
+                // handle success
+                return response.data.length;
+            })
+            .catch(function (error) {
+                expect(true).to.equal(false);
+            });
+        
+        return (c1==c2) ? true : false;
+    }
+
+    get().then((res) => {
+        expect( res ).to.equal( true );
+    });
   });
 });
