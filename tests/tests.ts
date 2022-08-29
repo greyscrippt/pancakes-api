5da89d2e835657c0bfb98263844de7096f966da5
Mongark <mongarkprogrammer@gmail.com>
2022-08-29 19:02:06 -0400

    :white_check_mark: Wrote test for RoomController.getCount() :white_check_mark:

--- tests/tests.ts
+++ tests/tests.ts
@@ -6,10 +6,27 @@ const API_URL = "http://localhost:3000/api/";
 
 describe("Connection test", () => {
   it("should return pong", donne => {
-    axios.get(API_URL+"ping/")
-        .then((result) => {
-            expect("pong").to.equal(result.data);
-            donne();
-        }).catch(error => donne(error));
+    axios
+        .get(API_URL+"ping/")
+            .then((result) => {
+                expect(result.data).to.equal("pong");
+                donne();
+            }).catch(error => donne(error));
   });
 });
+
+describe("Endpoint test for RoomController.getCount()", () => {
+    it("should return true", donne => {
+      axios
+          .get(API_URL+"rooms/getCount")
+              .then((result1) => {
+                  axios
+                    .get(API_URL+"rooms/getAll")
+                        .then((result2) => {
+                            expect(result1).to.equal(result2.data.length);
+                        });
+
+                  donne();
+              }).catch(error => donne(error));
+    });
+  });
