ed63f8ccb3aefbfdc58f4f502b4c74c2663d6bab
Mongark <mongarkprogrammer@gmail.com>
2022-09-03 16:34:22 -0400

    :white_check_mark: Fixed async tests for Room endpoints

--- tests/Rooms.tests.ts
+++ tests/Rooms.tests.ts
@@ -2,8 +2,6 @@ import { assert, expect, should } from "chai";
 import axios, { AxiosResponse } from "axios";
 import "mocha";
 
-import RoomModel from "../src/models/common/room";
-
 const API_URL = "http://localhost:3000/api/";
 
 const config = {
@@ -11,101 +9,84 @@ const config = {
 
 const client = axios(config);
 
-describe("Connection test", () => {
+describe("Connection test", async() => {
+    const data = await axios.get( API_URL+"ping/" );
 
     it("should return a 200 code", donne => {
-        axios.get( API_URL+"ping/" )
-            .then(( res ) => {
-                assert.equal( res.status, 200 );
-                donne();
-            }).catch((err) => donne(err));
+        assert.equal( data.status, 200 );
+        donne();
     });
 
     it("should return pong", donne => {
-        axios
-            .get( API_URL+"ping/" )
-                .then(( res ) => {
-                    assert.notEqual( res.data, undefined );
-                    assert.equal( res.data, "pong" );
-
-                    donne();
-                })
-                .catch(error => donne(error))
+        assert.notEqual( data.data, undefined );
+        assert.equal( data.data, "pong" );
     });
 });
 
 describe("RoomController.getAll()", () => {
+    let data: AxiosResponse<any, any>;
+
+    beforeEach(async() => {
+        data = await axios.get( API_URL+"rooms/getAll" );
+    });
 
     it("should return a 200 code", donne => {
-        axios.get( API_URL+"rooms/getAll" )
-            .then(( res ) => {
-                assert.equal( res.status, 200 );
-                donne();
-            }).catch((err) => donne(err));
+        assert.equal( data.status, 200 );
+        donne();
     });
 
 
     it("should return an array of type Room", donne => {
-        axios.get( API_URL+"rooms/getAll" )
-            .then((res) => {
-                assert.typeOf( res.data, 'array' );
-                donne();
-            }).catch((err) => donne(err) );
+        assert.typeOf( data.data, 'array' );
+        donne();
     });
 });
 
 describe("RoomController.getCount()", () => {
+    let count: AxiosResponse<any, any>;
+    let count2: AxiosResponse<any, any>;
+
+    beforeEach(async() => {
+        count = await axios.get( API_URL+"rooms/getCount" );
+        count2 = await axios.get( API_URL+"rooms/getAll" );
+    });
 
     it("should return a 200 code", donne => {
-        axios.get( API_URL+"rooms/getCount" )
-            .then(( res ) => {
-                assert.equal( res.status, 200 );
-                donne();
-            }).catch((err) => donne(err));
+        assert.equal( count.status, 200 );
+        donne();
     });
 
 
     it("should return a number", donne => {
-        axios.get( API_URL+"rooms/getCount" )
-            .then((res) => {
-                assert.typeOf( res.data, 'number' );
-                donne();
-            }).catch((err) => donne(err) );
+        assert.typeOf( count.data, 'number' );
+        donne();
     });
 
 
-    it("should compare results between /getCount and /getAll", donne => {
-        axios.get( API_URL+"rooms/getCount" )
-            .then(( res1 ) => {
-                axios.get( API_URL+"rooms/getAll" )
-                    .then(( res2 ) => {
-                        assert.equal( res1.data, res2.data.length );
+    it("should compare results between /getCount and /getAll", async donne => {
+        assert.equal( count.data, count2.data.length );
 
-                        donne();
-                    }).catch((err) => donne(err));
-            }).catch((err) => donne(err));
+        donne();
     });
 });
 
-describe("RoomController.postOne()", () => {
+describe("RoomController.postOne()", async() => {
+    let data: AxiosResponse<any, any>;
 
-    describe("connect", () => {
-        axios.post( API_URL+"rooms/post", { name: "My Very Own Test" } )
-            .then(( res ) => {
-
-                it("should return a 200 code", donne => {
-                    assert.equal( res.status, 200 );
-                    donne();
-                });
-                it("the result data should be of type 'object'", donne => {
-                    assert.equal( typeof res.data, "object" );
-                    donne();
-                });
-                it("the result data.id should be of type 'string'", donne => {
-                    assert.equal( typeof res.data.id, "string" );
-                    donne();
-                });
-
-            }).catch((err) => err);
+    beforeEach(async() => {
+        data = await axios.post( API_URL+"rooms/post", { name: "My Very Own Test" } );
+    });
+    
+    it("should return a 200 code", donne => {
+        assert.equal( data.status, 200 );
+        donne();
+    });
+    it("the result data should be of type 'object'", donne => {
+        assert.equal( typeof data.data, "object" );
+        donne();
+    });
+    it("the result data.id should be of type 'string'", donne => {
+        assert.equal( typeof data.data.id, "string" );
+        donne();
     });
 });
