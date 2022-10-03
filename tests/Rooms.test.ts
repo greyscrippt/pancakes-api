4961b7f60511d4b5e99a30793cad8e8aeb2afe1c
Mongark <mongarkprogrammer@gmail.com>
2022-10-03 15:26:58 -0400

    Refactored test structure

--- tests/Rooms.test.ts
+++ tests/Rooms.test.ts
@@ -3,30 +3,7 @@ import axios, { AxiosResponse } from "axios";
 
 const API_URL = "http://localhost:3000/api/";
 
-describe("Connection test", async() => {
-    let data: AxiosResponse<any, any>;
 
-    beforeEach(async() => {
-        data = await axios.get( API_URL+"ping" );
-        console.log( data.data );
-    });
-
-    it("data shouldn't be of type 'undefined'", donne => {
-        assert.notEqual( data, undefined );
-        donne();
-    });
-    
-    it("should return a 200 code", donne => {
-        assert.equal( data.status, 200 );
-        donne();
-    });
-
-    it("should return pong", donne => {
-        assert.notEqual( data.data, undefined );
-        assert.equal( data.data, "pong" );
-        donne();
-    });
-});
 
 describe("RoomController.getAll()", () => {
     let data: AxiosResponse<any, any>;
