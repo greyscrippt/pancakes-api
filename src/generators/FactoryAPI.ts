3b25b5b912a2274e5f720d0fcc09acf253888282
Mongark <mongarkprogrammer@gmail.com>
2022-11-04 14:06:25 -0400

    Created Service factory class

--- src/generators/FactoryAPI.ts
+++ src/generators/FactoryAPI.ts
@@ -4,3 +4,18 @@
  * 
  */
 
+import { Request, Response } from "express";
+import { Model } from "mongoose";
+
+interface Endpoint {
+    body: string,
+    type: string,
+}
+
+class ServiceFactory {
+    async create(endpoint: Endpoint, model: Model<any>) {
+        if(endpoint.type == "GET_ONE") {
+            return await model.findById( endpoint.body );
+        } 
+    }
+}
