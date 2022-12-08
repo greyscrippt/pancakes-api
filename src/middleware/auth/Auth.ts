ff025f50783dbe009dd76a6fe11e85a8dd403087
Mongark <mongarkprogrammer@gmail.com>
2022-12-08 12:27:20 -0400

    "Added" verifyToken function to "Auth"

--- src/middleware/auth/Auth.ts
+++ src/middleware/auth/Auth.ts
@@ -2,7 +2,13 @@ import { NextFunction, Request, Response } from "express";
 
 import UserModel from "../../data/models/UserModel";
 
-import TokenManager from "./token-manager";
+async function signToken(req: Request, res: Response, next: NextFunction) {
+    const user_data = req.body;
+
+    if(!user_data) {
+        res.status(401).send("User data is not provided");
+    }
+}
 
 async function register(req: Request, res: Response, next: NextFunction) {
     const { username, password } = req.body
