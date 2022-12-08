f6f1cf8546317768e05d95e087e7a6fd1629c1d3
Mongark <mongarkprogrammer@gmail.com>
2022-12-08 15:51:46 -0400

    Implemented test for token generation and validation

---
+++ src/middleware/auth/TokenManager.ts
@@ -0,0 +1,23 @@
+import jwt, {Secret} from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+function generateToken(data: any) {
+    if(!data) return;
+
+    return jwt.sign(data, process.env.TOKEN_SECRET);
+}
+
+function validateToken(data: any) {
+    if(!data) return;
+
+    return jwt.verify(data, process.env.TOKEN_SECRET);
+}
+
+const TokenManager = {
+    generateToken,
+    validateToken,
+};
+
+export default TokenManager;
--- tests/meta/Auth.test.ts
+++ tests/meta/Auth.test.ts
@@ -1,7 +1,18 @@
+import {assert} from "chai";
 import supertest from "supertest";
 import app from "../../src/app";
+import TokenManager from "../../src/middleware/auth/TokenManager";
 
 describe("Tests for authentication functions", () => {
+    const token_data = { test: "123" };
+
+    it("testing token generation and validation", () => {
+        const token: any = TokenManager.generateToken(token_data);
+        const verified: any = TokenManager.validateToken(token);
+
+        assert.isOk(verified);
+        assert.equal(verified.test, token_data.test);
+    });
 });
 
 describe("Authentication endpoint tests", () => {
