7c3c0e6f28e281f3b4edc9fec0dcb9f49263e3c2
Mongark <mongarkprogrammer@gmail.com>
2022-12-08 12:21:43 -0400

    Removed routes associated with token-manager.

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,14 +1,14 @@
 // TODO: organize json payload in CRUD
 
 import { Router } from "express";
-import TokenManager from "../middleware/auth/token-manager";
+// import TokenManager from "../middleware/auth/token-manager";
 
 import * as Auth from "../middleware/auth/Auth";
 
 const UserRoutes = Router();
 
 UserRoutes.post("/register", Auth.register);
-UserRoutes.post("/login", TokenManager.signToken);
-UserRoutes.post("/verifyToken", TokenManager.verifyToken);
+// UserRoutes.post("/login", TokenManager.signToken);
+// UserRoutes.post("/verifyToken", TokenManager.verifyToken);
 
 export default UserRoutes;
