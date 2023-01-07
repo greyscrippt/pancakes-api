4ada31efcb22dacb4d6fcb0a5cfab4d7bbd19ed9
Mongark <mongarkprogrammer@gmail.com>
2023-01-07 15:50:27 -0400

    Added testEndpoint function to TestFactory

--- test-factory/TestFactory.ts
+++ test-factory/TestFactory.ts
@@ -1,5 +1,11 @@
 import RouteType from "../src/generators/CommonTypes/RouteType";
 
 export function testRoute(route: Array<RouteType>) {
+    route.map(( endpoint: RouteType ) => {
+        testEndpoint(endpoint);
+    });
+}
+
+export function testEndpoint(endpoint: RouteType) {
 
 }
