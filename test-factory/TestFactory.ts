5f3134e7323beb50b9eba258da5191afb6f1b6c8
Mongark <mongarkprogrammer@gmail.com>
2023-01-09 20:16:01 -0400

    Remade the testing strategy

--- test-factory/TestFactory.ts
+++ test-factory/TestFactory.ts
@@ -4,6 +4,23 @@ import supertest from "supertest";
 import RouterFactory from "../src/generators/Factories/RouterFactory";
 import RouterConfig from "../src/generators/CommonTypes/RouterConfig";
 
+const EndpointFunctions = {
+    "GET": {
+        "GET_ALL": () => {},
+        "GET_BY_ID": () => {},
+        "GET_COUNT": () => {},
+    },
+    "DELETE": {
+        "DELETE_BY_ID": () => {},
+    },
+    "UPDATE": {
+        "UPDATE_BY_ID": () => {},
+    },
+    "POST": {
+        "POST_ONE": () => {},
+    },
+};
+
 export function testRouter(router_config: Array<RouterConfig>) {
     const router = RouterFactory.createRoute(router_config);
     const express_app = express();
@@ -13,17 +30,9 @@ export function testRouter(router_config: Array<RouterConfig>) {
     const app = supertest(express_app);
 
     router_config.map((endpoint: RouterConfig) => {
-        if(endpoint.type == "GET") {
-            it("test for a GET endpoint", () => {});
-        } else if(endpoint.type == "POST") {
-            it("test for a POST endpoint", () => {});
-        } else if(endpoint.type == "PATCH") {
-            it("test for a PATCH endpoint", () => {});
-        } else if(endpoint.type == "DELETE") {
-            it("test for a DELETE endpoint", () => {});
-        }
+        // EndpointFunctions.find((item: any) => item.type == type).action(model);
     });
 }
 
-export function testEndpoint(endpoint: RouterConfig) {
+export function testEndpoint(endpoint: RouterConfig, app: any) {
 }
