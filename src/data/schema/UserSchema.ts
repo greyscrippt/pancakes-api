2a004b7235bf6d2d6f318162ae07164ddac4ac30
Mongark <mongarkprogrammer@gmail.com>
2023-01-18 20:09:35 -0400

    Updated UserSchema

--- src/data/schema/UserSchema.ts
+++ src/data/schema/UserSchema.ts
@@ -3,6 +3,10 @@ import { Schema } from "mongoose";
 const UserSchema = new Schema({
     username: {
         type: String,
+        required: true,
+    },
+    email: {
+        type: String,
         unique: true,
         required: true,
     },
