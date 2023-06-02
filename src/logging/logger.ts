6dfbd13dcef02e740915f6d0e90cebb7ffe61c28
greyscrippt <greyscrippt@gmail.com>
2023-06-02 17:15:59 -0400

    Styled logger

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,7 +1,14 @@
 import winston from "winston";
 
 const logger = winston.createLogger({
-    transports: [ new winston.transports.Console() ],
+    transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+          )
+        })
+    ],
 });
 
 export default logger;
